121a9f3877bc04e74012f5cef75e3040302b7cbd
dan850 <dan06982@gmail.com>
2022-10-14 18:07:36 -0500

    insert animation

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ const game = () => {
       const playerHand = document.querySelector(".player-hand");
       const computerHand = document.querySelector(".computer-hand");
       const hands = document.querySelectorAll(".hands img");
-
+      
+      
+      hands.forEach(hand => {
+        hand.addEventListener("animationend", function() {
+          this.style.animation = "";
+        });
+      });
 
       //Computer Options
       const computerOptions = ["rock", "paper", "scissors"];
@@ -41,9 +47,16 @@ const game = () => {
 
           compareHands(this.textContent, computerChoice);
 
-//Update Images
-playerHand.src = `./assets/${this.textContent}.png`;
-computerHand.src = `./assets/${computerChoice}.png`;
+          setTimeout(() => {
+            //Here is where we call compare hands
+            compareHands(this.textContent, computerChoice);
+            //Update Images
+            playerHand.src = `./assets/${this.textContent}.png`;
+            computerHand.src = `./assets/${computerChoice}.png`;
+          }, 2000);
+          //Animation
+          playerHand.style.animation = "shakePlayer 2s ease";
+          computerHand.style.animation = "shakeComputer 2s ease";
 
 });
 });
