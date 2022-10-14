fe43a17923d3b5c65e0d423186ab55474a71b8c8
dan850 <dan06982@gmail.com>
2022-10-14 01:29:48 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+  
+    //Start the Game
+    const startGame = () => {
+      const playBtn = document.querySelector(".intro button");
+      const introScreen = document.querySelector(".intro");
+      const match = document.querySelector(".match");
+  
+      playBtn.addEventListener("click", () => {
+        introScreen.classList.add("fadeOut");
+        match.classList.add("fadeIn");
+      });
+    };
+
+//Is call all the inner function
+startGame();
+};
+    //start the game function
+  game();
