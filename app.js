b5acfcf13c7fd5b897cddaf9bc991befda3f82d0
dan850 <dan06982@gmail.com>
2022-10-14 16:43:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const game = () => {
     let pScore = 0;
     let cScore = 0;
-  
+
     //Start the Game
     const startGame = () => {
       const playBtn = document.querySelector(".intro button");
@@ -23,8 +23,24 @@ const game = () => {
       const computerHand = document.querySelector(".computer-hand");
       const hands = document.querySelectorAll(".hands img");
 
+
+      //Computer Options
+      const computerOptions = ["rock", "paper", "scissors"];
+
+
+      options.forEach(option => {
+        option.addEventListener("click", function() {
+          //Computer Choice
+          const computerNumber = Math.floor(Math.random() * 3);
+          const computerChoice = computerOptions[computerNumber];
+          console.log(computerChoice);
+
+});
+});
+  };
 //Is call all the inner function
 startGame();
+playMatch();
 };
     //start the game function
   game();
